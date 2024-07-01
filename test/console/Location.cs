64cb52c697cb8451a6ee7d4f2538dbf0b3576889
Lyle Dodge <16616380+lyledodgegh@users.noreply.github.com>
2024-06-30 20:20:34 -0400

    Update Location.cs

--- test/console/Location.cs
+++ test/console/Location.cs
@@ -8,3 +8,40 @@ public class Location
     [JsonPropertyName("description")]
     public string Description { get; set;}
 }
+
+// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class AvailabilityZoneMapping
+    {
+        public string logicalZone { get; set; }
+        public string physicalZone { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string geography { get; set; }
+        public string geographyGroup { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public List<PairedRegion> pairedRegion { get; set; }
+        public string physicalLocation { get; set; }
+        public string regionCategory { get; set; }
+        public string regionType { get; set; }
+    }
+
+    public class PairedRegion
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Root
+    {
+        public List<AvailabilityZoneMapping> availabilityZoneMappings { get; set; }
+        public string displayName { get; set; }
+        public string id { get; set; }
+        public Metadata metadata { get; set; }
+        public string name { get; set; }
+        public string regionalDisplayName { get; set; }
+        public string type { get; set; }
+    }
+
